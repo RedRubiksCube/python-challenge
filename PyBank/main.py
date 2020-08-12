78b4e05333819fc6ffaf735452d8db890258d05b
RedRubiksCube <45070667+RedRubiksCube@users.noreply.github.com>
2020-08-12 13:02:18 -0400

    totals working

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,22 +4,21 @@ import csv
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 #This will turn total months into a list 
-revenue = 0
 def calculations (data):
-    #months = str(data[0])
-    #profit = int(data[1])
+    months = str(data[0])
+    profit = int(data[1])
 
     #Gets total amount of rows    
     month_count = (sum(1 for row in csvreader) + 1)
     
     #Totals Monthly profit/loss
-    revenue_total = sum(int(data[1]) for row in csvreader)
+    revenue_total = sum(float(profit))
 
     #All print statements
     print(month_count) #Should be 86
     
     #Print Monthly revenue
-    #print(total) #Total Profil/Loss should be 38,382,578
+    #print(reveune_total) #Total Profil/Loss should be 38,382,578
 
 
 #Loop that goes through each row of data in the csv file
@@ -27,7 +26,24 @@ with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
        
     header = next(csvreader) #excludes header column
-    #row = next(csvreader)
+    
+    #######
+    #I know the code below works for sure - with accurate data
+    #csvdata = list(csvreader)
+    #row_count = len(csvdata)
+    #print(row_count)
+    #######
+    
+
+       
+    monthly_rev = [] 
     for row in csvreader:
-        P_F = calculations(row)
+        values = row[1] 
+        monthly_rev.append(values)          
+    monthly_rev_list = [float(integral) for integral in monthly_rev] 
+    totals3 = sum(monthly_rev_list) 
+    
+    print(totals3)
+    #for row in csvreader:
+    #    calculations(row)
     
