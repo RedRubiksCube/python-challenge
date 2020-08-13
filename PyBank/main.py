0b6590cc51756c5a662a6302598edbe3cf647945
RedRubiksCube <45070667+RedRubiksCube@users.noreply.github.com>
2020-08-13 18:36:48 -0400

    Successful in totalling months and revenue

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,46 +3,41 @@ import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-#This will turn total months into a list 
-def calculations (data):
-    months = str(data[0])
-    profit = int(data[1])
-
-    #Gets total amount of rows    
-    month_count = (sum(1 for row in csvreader) + 1)
-    
-    #Totals Monthly profit/loss
-    #revenue_total = sum(float(profit))
-
-    #All print statements
-    print(month_count) #Should be 86
-    
-    #Print Monthly revenue
-    #print(reveune_total) #Total Profil/Loss should be 38,382,578
-
-
 #Loop that goes through each row of data in the csv file
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
        
-    header = next(csvreader) #excludes header column
-    
-    #######
-    #I know the code below works for sure - with accurate data
-    csvdata = list(csvreader)
-    row_count = len(csvdata)
-    print(row_count)
-    #######
-    
+    header = next(csvreader) #excludes header column 
+
     # Code below calculates the total amount revenue   
-    monthly_rev = [] 
+    monthly_rev = []
+    monthly_rev_diff = [] 
+    tot_month = []
+    month_change_average = []
     for row in csvreader:
         values = row[1] 
-        monthly_rev.append(values)          
+        months = row[0]
+        monthly_rev.append(values)
+        
+        tot_month.append(months)          
+    #The two lines below this get the total revenue for the given period
+    #They reference the monthly_rev list 
     monthly_rev_list = [float(integral) for integral in monthly_rev] 
     totals3 = sum(monthly_rev_list) 
     
-    #print(totals3)
-    #for row in csvreader:
-    #    calculations(row)
+
+    for i in range(len(monthly_rev)):
+        j = int(i - 1)
+        if j > 0:
+            diff = (int(monthly_rev[j]) - int(monthly_rev[i]))
+            monthly_rev_diff.append(diff)
+        
+    avg = sum(monthly_rev_diff)/len(monthly_rev_diff)   
+          
+
     
+    
+    #All prints
+    print(len(tot_month))
+    print(totals3)
+    print(avg)
